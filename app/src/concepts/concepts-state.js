55bc11a79fe8766d7265522f3e7b15f4b2cbf374
Luke Machowski <luke@synthesis.co.za>
2015-09-30 11:05:38 +0200

    Now we import the html for the concepts module using the package relative path.

--- app/src/concepts/concepts-state.js
+++ app/src/concepts/concepts-state.js
@@ -1,9 +1,12 @@
+import viewTemplate from 'concepts/concepts-view.html!text';
+
 export default {
   name: 'concepts',
   url: '/concepts',
   views: {
     '': {
       templateUrl: "/src/concepts/concepts-view.html",
+      template: viewTemplate,
       controller: "ConceptsController",
       controllerAs: 'controller'
     }
