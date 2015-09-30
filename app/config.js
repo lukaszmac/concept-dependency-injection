21e55899f4787e757edf115636e99714a80eca31
Luke Machowski <luke@synthesis.co.za>
2015-09-30 11:04:35 +0200

    Updated traceur version. CLI was complaining.

--- app/config.js
+++ app/config.js
@@ -20,8 +20,8 @@ System.config({
     "css": "github:systemjs/plugin-css@0.1.18",
     "json": "github:systemjs/plugin-json@0.1.0",
     "text": "github:systemjs/plugin-text@0.0.2",
-    "traceur": "github:jmcriffey/bower-traceur@0.0.90",
-    "traceur-runtime": "github:jmcriffey/bower-traceur-runtime@0.0.90",
+    "traceur": "github:jmcriffey/bower-traceur@0.0.91",
+    "traceur-runtime": "github:jmcriffey/bower-traceur-runtime@0.0.91",
     "github:angular-ui/ui-router@0.2.15": {
       "angular": "github:angular/bower-angular@1.4.6"
     },
